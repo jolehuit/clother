fd6180325bbf147031dc6cf4d8bc5f2f51715d76
jolehuit <130084799+jolehuit@users.noreply.github.com>
2026-03-11 18:32:50 +0100

    chore: bump to v2.9.4

--- clother.sh
+++ clother.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 IFS=$'\n\t'
 umask 077
 
-readonly VERSION="2.9.3"
+readonly VERSION="2.9.4"
 readonly CLOTHER_DOCS="https://github.com/jolehuit/clother"
 
 # =============================================================================
